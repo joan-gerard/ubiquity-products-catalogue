7390abb568daf4361b7e56f5cbdacac4977f7eb6
Joan Gerard <joan.gerard@outlook.com>
2022-04-13 21:04:31 +0200

    add logic to clear search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './SearchBar.css';
 import clear from '../../assets/close-icon.svg';
 import defaultGrid from '../../assets/default-grid.svg';
 import defaultList from '../../assets/default-list.svg';
 
 type SearchBarProps = {
-  switchToGrid: () => void ;
-  switchToView: () => void ;
+  switchToGrid: () => void;
+  switchToView: () => void;
 }
 
 
 const SearchBar: React.FC<SearchBarProps> = (props) => {
 
-  
+  let searchInputRef = useRef<HTMLInputElement>(null);
+
+  const clearSearchInput = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchInputRef?.current?.value) {
+
+      searchInputRef.current.value = '';
+    }
+  }
+
   return (
     <nav className="nav-bar">
       <form className="search-bar">
-        <input className="search-bar__input" type="search" placeholder="Search" />
-        <button className="search-bar__clear"><img src={clear} alt="clear search" /></button>
+        <input className="search-bar__input" type="search" placeholder="Search" ref={searchInputRef} />
+        <button onClick={(e) => clearSearchInput(e)} className="search-bar__clear"><img src={clear} alt="clear search" /></button>
       </form>
       <div className="views-filters">
         <img onClick={props.switchToView} src={defaultList} alt="List View Icon" />
